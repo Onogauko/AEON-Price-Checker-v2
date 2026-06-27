abba92d587493ce098d04ef059ead850c1887a97
Onogauko <156558403+Onogauko@users.noreply.github.com>
2026-06-27 09:59:51 +0700

    router.js

--- js/router.js
+++ js/router.js
@@ -1,5 +1,6 @@
 import { renderHome } from "./pages/home.js";
 import { renderSettings } from "./pages/settings.js";
+import { renderDownload } from "./pages/download.js";
 
 export function navigate(page) {
 
@@ -9,6 +10,10 @@ export function navigate(page) {
 
         case "settings":
             app.innerHTML = renderSettings();
+            break;
+
+        case "download":
+            app.innerHTML = renderDownload();
             break;
 
         default:
