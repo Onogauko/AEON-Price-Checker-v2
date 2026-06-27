9d5e61e8321fe2caaad516d80d555679647ff5fd
Onogauko <156558403+Onogauko@users.noreply.github.com>
2026-06-27 10:04:15 +0700

    download.js

--- js/pages/download.js
+++ js/pages/download.js
@@ -1,73 +1,19 @@
-import { navigate } from "../router.js";
+async function startDownload() {
 
-export function renderDownload() {
+    const progressBar = document.getElementById("progressBar");
 
-    setTimeout(() => {
+    const progressText = document.getElementById("progressText");
 
-        document
-        .getElementById("btnBack")
-        ?.addEventListener("click", () => {
+    for (let i = 0; i <= 100; i++) {
 
-            navigate("home");
+        progressBar.value = i;
 
-        });
+        progressText.innerHTML = "Downloading... " + i + "%";
 
-    },0);
+        await new Promise(resolve => setTimeout(resolve, 30));
 
-    return `
+    }
 
-    <div class="min-h-screen bg-gray-100">
-
-        <div class="bg-red-700 text-white p-5 shadow">
-
-            <button id="btnBack">
-
-                ← Back
-
-            </button>
-
-            <h1 class="text-2xl font-bold mt-3">
-
-                Download Database
-
-            </h1>
-
-        </div>
-
-        <div class="p-5">
-
-            <button
-                id="btnDownload"
-                class="bg-red-700 text-white rounded-lg p-4 w-full">
-
-                Download Product Database
-
-            </button>
-
-            <div class="mt-6">
-
-                <progress
-                    id="progressBar"
-                    value="0"
-                    max="100"
-                    class="w-full">
-
-                </progress>
-
-                <p
-                    id="progressText"
-                    class="mt-3">
-
-                    Ready
-
-                </p>
-
-            </div>
-
-        </div>
-
-    </div>
-
-    `;
+    progressText.innerHTML = "✅ Download Complete";
 
 }
